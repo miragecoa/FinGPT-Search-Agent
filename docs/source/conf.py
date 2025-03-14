e5837614fd19955cb81289c2a70361c2e92f6377
Keyi Wang <364059176w@gmail.com>
2025-03-14 18:28:41 -0500

    change the format

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,12 +14,23 @@ release = '3.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+# extensions = [
+#     'sphinx.ext.autodoc',
+#     'sphinx.ext.napoleon',
+#     'sphinx.ext.viewcode',
+#     'sphinx.ext.intersphinx',
+#     'sphinx.ext.mathjax'
+# ]
+
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.mathjax'
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.mathjax",
+    "sphinx_rtd_theme",
+    "nbsphinx",
+    "nbsphinx_link",
 ]
 
 templates_path = ['_templates']
@@ -27,9 +38,37 @@ exclude_patterns = []
 
 master_doc = 'index'
 
+numfig = True
+math_numfig = True
+
+language = 'en'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+# html_static_path = ['_static']
+
+html_theme = "sphinx_rtd_theme"
+
 html_static_path = ['_static']
+html_css_files = [
+    'css/custom.css',
+]
+
+html_context = {
+    "display_github": True, # Integrate GitHub
+    "github_user": "Open-Finance-Lab", # Username
+    "github_repo": "FinRL-Contest", # Repo name
+    "github_version": "main", # Version
+    "conf_py_path": "/docs/source/", # Path in the checkout to the docs root
+}
+
+latex_elements = {
+    'preamble': r'''
+    \usepackage{amsmath}
+    \usepackage{braket}
+    \usepackage{algorithm}
+    \usepackage{algorithmic}
+    '''
+}
