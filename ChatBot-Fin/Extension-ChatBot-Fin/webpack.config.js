471971b68aa8319954218514fd53d9ebb7bc3577
Felix Tian <112596144+FlyM1ss@users.noreply.github.com>
2025-04-09 15:27:17 -0400

    Updated to fully use webpack

--- ChatBot-Fin/Extension-ChatBot-Fin/webpack.config.js
+++ ChatBot-Fin/Extension-ChatBot-Fin/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/content_archive.js',
+    entry: './src/main.js',
     mode: 'production',
     output: {
         filename: 'bundle.js',
